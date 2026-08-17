c9ce55a48f01b3f78579e20872afab8be97764bb
Shahzod O. Urinboyev <shahzod.urinboyev@uzinfocom.uz>
2026-08-17 17:07:29 +0500

    fix: default new sessions to Uzbek

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,8 +3,16 @@ import { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
 export function proxy(req: NextRequest) {
+  const hasLocaleCookie = req.cookies.has("NEXT_LOCALE");
+  if (!hasLocaleCookie) {
+    req.cookies.set("NEXT_LOCALE", routing.defaultLocale);
+  }
+
   const intlMiddleware = createMiddleware(routing);
   const response = intlMiddleware(req);
+  if (!hasLocaleCookie) {
+    response.cookies.set("NEXT_LOCALE", routing.defaultLocale);
+  }
 
   return response;
 }
