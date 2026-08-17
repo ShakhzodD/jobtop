1f1c06853adb7b768b670f58f0f5036e0d71e9a6
Shahzod O. Urinboyev <shahzod.urinboyev@uzinfocom.uz>
2026-08-17 16:06:56 +0500

    style: show navigation progress spinner

--- src/shared/providers/BProgressProvider.tsx
+++ src/shared/providers/BProgressProvider.tsx
@@ -8,9 +8,8 @@ export default function BProgressProvider({ children }: PropsWithChildren) {
     <ProgressProvider
       color="#087f51"
       height="3px"
-      options={{ parent: "#jobtop-page", showSpinner: false }}
+      options={{ parent: "#jobtop-page", showSpinner: true }}
       shallowRouting
-      disableSameURL
     >
       {children}
     </ProgressProvider>
